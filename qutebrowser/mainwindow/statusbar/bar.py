5a043d2fb8421753acb555867a4502c775addcee
Jimmy <jimmy@spalge.com>
2018-07-29 15:08:40 +1200

    Add statusbar widget to show boolean settings
    Adds a new widget that can be displayed in the statusbar to show the
    state of arbitrary boolean settings for the current URL. Some boolean
    settings actually support a third value `ask`. Those are partially
    supported in that since the setting isn't explicitly configured for that
    URL then it will show as not activated. I suspect users would prefer it
    show the actual activation state of that setting for the current tab
    though. Since, presumably, all of the BoolAsk type settings map to an
    attribute in `websettings.AbstractSettings` we could use `testAttribute`
    to check that.
    
    It maps settings to "indicators", which are arbitrary strings, expected
    to a single character. If the setting is enabled for the current URL the
    indicator is shown in the widget, if not then it isn't. For example if
    your `settings.widget.content` setting was
    
        '["S|content.javascript.enabled", "G|content.webgl", "L|content.geolocation",]'
    
    You would have an additional widget before the scroll indicator that
    would show something like `[]` if the page didn't have javascript
    enabled or webgl enabled and geolocation was either false or ask. Or it
    might show `[SL]` if javascript and geolocation were enabled.
    
    I tested to see if unicode symbols like ROUND PUSHPIN could be used (for
    a geolocation indicator) but that doesn't work because:
    
    * if using autoconfig.yml pyYAML complains
    * if using config.py `BaseType._validate_surrogate_escapes()` complains
      about surrogate escapes. And where that is printed out from claims
      json.dump can't handle them but `json.loads(json.dumps("📍"))` works
      just fine.
    
    Adds a new config option set statusbar settings widget
    
    It's a list of strings. Strings should be in a form like
    `S|content.javascript.enabled` where `S` will be shown when javascript is
    enabled for the current page and wont be shown when it isn't.
    
    The `ValueError` try/catch in `parse_config()` catches both when the
    specified sitting isn't a boolean one and the the string didn't have a
    `|` in it so the split failed.

--- qutebrowser/mainwindow/statusbar/bar.py
+++ qutebrowser/mainwindow/statusbar/bar.py
@@ -32,7 +32,7 @@ from qutebrowser.keyinput import modeman
 from qutebrowser.utils import usertypes, log, objreg, utils
 from qutebrowser.mainwindow.statusbar import (backforward, command, progress,
                                               keystring, percentage, url,
-                                              tabindex, textbase)
+                                              tabindex, textbase, settings)
 
 
 @dataclasses.dataclass
@@ -144,6 +144,7 @@ class StatusBar(QWidget):
         url: The UrlText widget in the statusbar.
         prog: The Progress widget in the statusbar.
         cmd: The Command widget in the statusbar.
+        settings: The BooleanSetting widget in the statusbar.
         _hbox: The main QHBoxLayout.
         _stack: The QStackedLayout with cmd/txt widgets.
         _win_id: The window ID the statusbar is associated with.
@@ -200,6 +201,7 @@ class StatusBar(QWidget):
         self.tabindex = tabindex.TabIndex()
         self.keystring = keystring.KeyString()
         self.prog = progress.Progress(self)
+        self.settings = settings.BooleanSettings(self, self._win_id)
         self._draw_widgets()
 
         config.instance.changed.connect(self._on_config_changed)
@@ -216,14 +218,14 @@ class StatusBar(QWidget):
             self._set_hbox_padding()
         elif option == 'statusbar.widgets':
             self._draw_widgets()
+        self.settings.on_config_changed(option)
 
-    def _draw_widgets(self):
+    def _draw_widgets(self):  # noqa: C901 pragma: no mccabe
         """Draw statusbar widgets."""
         # Start with widgets hidden and show them when needed
         for widget in [self.url, self.percentage,
                        self.backforward, self.tabindex,
-                       self.keystring, self.prog]:
-            assert isinstance(widget, QWidget)
+                       self.keystring, self.prog, self.settings]:
             widget.hide()
             self._hbox.removeWidget(widget)
 
@@ -257,6 +259,11 @@ class StatusBar(QWidget):
                 self.prog.enabled = True
                 if tab:
                     self.prog.on_tab_changed(tab)
+            elif segment == 'settings':
+                self._hbox.addWidget(self.settings)
+                self.settings.show()
+                if tab:
+                    self.settings.on_tab_changed(tab)
 
     @pyqtSlot()
     def maybe_hide(self):
@@ -396,6 +403,7 @@ class StatusBar(QWidget):
         self.prog.on_tab_changed(tab)
         self.percentage.on_tab_changed(tab)
         self.backforward.on_tab_changed(tab)
+        self.settings.on_tab_changed(tab)
         self.maybe_hide()
         assert tab.is_private == self._color_flags.private
 

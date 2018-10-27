859c2c8d25d5793dabb6b59c00769fc6f961fa22
Jimmy <jimmy@spalge.com>
2018-10-27 16:50:57 +1300

    Move permissions _widget setting to parent.

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -1392,7 +1392,6 @@ class WebEngineTab(browsertab.AbstractTab):
     def _set_widget(self, widget):
         # pylint: disable=protected-access
         super()._set_widget(widget)
-        self._permissions._widget = widget
         self._scripts._widget = widget
 
     def _install_event_filter(self):

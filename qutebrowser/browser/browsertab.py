32f018630052975ab2e033ac5ea0c1a001afd958
Jimmy <jimmy@spalge.com>
2019-03-25 13:33:25 +1300

    Move abort_questions to AbstractTab.
    I'm shortly going to pull the permissions stuff for the two backends to
    a common base class, they will both be able to make use of this.

--- qutebrowser/browser/browsertab.py
+++ qutebrowser/browser/browsertab.py
@@ -908,6 +908,8 @@ class AbstractTab(QWidget):
     fullscreen_requested = pyqtSignal(bool)
     #: Signal emitted before load starts (URL as QUrl)
     before_load_started = pyqtSignal(QUrl)
+    #: Signal emitted when a new load started or we're shutting down.
+    abort_questions = pyqtSignal()
 
     # Signal emitted when a page's load status changed
     # (argument: usertypes.LoadStatus)
@@ -959,6 +961,8 @@ class AbstractTab(QWidget):
             setattr, self, 'pending_removal', True))
 
         self.before_load_started.connect(self._on_before_load_started)
+        self.shutting_down.connect(self.abort_questions)  # type: ignore[arg-type]
+        self.load_started.connect(self.abort_questions)  # type: ignore[arg-type]
 
     def _set_widget(self, widget: QWidget) -> None:
         # pylint: disable=protected-access
--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -25,7 +25,7 @@ import re
 import html as html_utils
 import typing
 
-from PyQt5.QtCore import (pyqtSignal, pyqtSlot, Qt, QPoint, QPointF, QUrl,
+from PyQt5.QtCore import (pyqtSlot, Qt, QPoint, QPointF, QUrl,
                           QTimer, QObject)
 from PyQt5.QtNetwork import QAuthenticator
 from PyQt5.QtWidgets import QApplication, QWidget
@@ -1343,14 +1343,7 @@ class WebEngineTabPrivate(browsertab.AbstractTabPrivate):
 
 class WebEngineTab(browsertab.AbstractTab):
 
-    """A QtWebEngine tab in the browser.
-
-    Signals:
-        abort_questions: Emitted when a new load started or we're shutting
-            down.
-    """
-
-    abort_questions = pyqtSignal()
+    """A QtWebEngine tab in the browser."""
 
     def __init__(self, *, win_id, mode_manager, private, parent=None):
         super().__init__(win_id=win_id,
@@ -1853,10 +1846,6 @@ class WebEngineTab(browsertab.AbstractTab):
         page.loadFinished.connect(self._on_load_finished)
 
         self.before_load_started.connect(self._on_before_load_started)
-        self.shutting_down.connect(
-            self.abort_questions)  # type: ignore[arg-type]
-        self.load_started.connect(
-            self.abort_questions)  # type: ignore[arg-type]
 
         # pylint: disable=protected-access
         self.audio._connect_signals()

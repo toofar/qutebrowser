82cb656e0e78f3482a09eca929b9c461e0e9c483
Jimmy <jimmy@spalge.com>
2019-05-12 19:58:53 +1200

    Log warning when loading extensions.

--- qutebrowser/extensions/loader.py
+++ qutebrowser/extensions/loader.py
@@ -145,6 +145,14 @@ def walk_extensions() -> typing.Iterator[ExtensionInfo]:
     else:
         return
 
+    if any(os.listdir(ext_dir)):
+        log.extensions.warning(
+            "Loading EXTENSIONS in {}: this in an "
+            "EXPERIMENTAL FEATURE.".format(ext_dir)
+        )
+    else:
+        return
+
     prefix = 'qutebrowser.extensions.third_party.'
     for finder, name, _ispkg in pkgutil.walk_packages(
             path=[ext_dir],

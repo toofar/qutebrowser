5fcfb9f6166473e7414897309ae940b65205c76e
Jimmy <jimmy@spalge.com>
2019-05-26 18:26:09 +1200

    Use now tab-local interceptor registration.
    The profile registration method is now marked obsolete (for running in
    the IO thread): https://doc-snapshots.qt.io/qt5-dev/qwebengineprofile-obsolete.html#setRequestInterceptor
    
    I could have sworn I also got a warning on the console about us using
    the old API too but I don't see it anymore. Not sure if it is a one time
    thing or I imagined it.
    
    There is also a new `profile.setUrlRequestInterceptor()` which doesn't
    run in the IO thread. So putting the version check in the interceptor
    would be simpler but I have plans for the per-page one.

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -46,6 +46,7 @@ from qutebrowser.qt import sip
 
 
 _qute_scheme_handler = None
+_request_interceptor = None
 
 
 def init():
@@ -64,9 +65,12 @@ def init():
 
     log.init.debug("Initializing request interceptor...")
     req_interceptor = interceptor.RequestInterceptor(parent=app)
-    req_interceptor.install(webenginesettings.default_profile)
-    if webenginesettings.private_profile:
-        req_interceptor.install(webenginesettings.private_profile)
+    if not qtutils.version_check('5.13'):
+        req_interceptor.install(webenginesettings.default_profile)
+        if webenginesettings.private_profile:
+            req_interceptor.install(webenginesettings.private_profile)
+    else:
+        _request_interceptor = req_interceptor
 
     log.init.debug("Initializing QtWebEngine downloads...")
     download_manager = webenginedownloads.DownloadManager(parent=app)
@@ -1388,6 +1392,8 @@ class WebEngineTab(browsertab.AbstractTab):
         super()._set_widget(widget)
         self._permissions._widget = widget
         self._scripts._widget = widget
+        if qtutils.version_check('5.13'):
+            self._widget.page().setUrlRequestInterceptor(_request_interceptor)
 
     def _install_event_filter(self):
         fp = self._widget.focusProxy()

729b73f02bee05684e4d84b3a73bd9dc77a836f0
Florian Bruhin <me@the-compiler.org>
2022-04-20 16:16:13 +0200

    qt6 tests: Fix another webengine core move

--- tests/helpers/fixtures.py
+++ tests/helpers/fixtures.py
@@ -177,7 +177,7 @@ def testdata_scheme(qapp):
     try:
         global _qute_scheme_handler
         from qutebrowser.browser.webengine import webenginequtescheme
-        from qutebrowser.qt.webenginewidgets import QWebEngineProfile
+        from qutebrowser.qt.webenginecore import QWebEngineProfile
         webenginequtescheme.init()
         _qute_scheme_handler = webenginequtescheme.QuteSchemeHandler(
             parent=qapp)
